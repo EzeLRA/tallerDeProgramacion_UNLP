374d9f470b8a6c1303e00474d6774c2ce7948da5
EzeLRA <ezeleocraft@gmail.com>
2024-08-27 17:05:45 -0300

    Practica de Arboles

--- Pascal - Imperativo/Practica 3/ejercicio2.pas
+++ Pascal - Imperativo/Practica 3/ejercicio2.pas
@@ -61,9 +61,9 @@ end;
 
 procedure imprimirVenta(v:venta);
 begin
-	writeln(v.codigoProducto);
-	writeln(v.fecha);
-	writeln(v.unidadesVendidas);
+	writeln('Codigo Producto: ',v.codigoProducto);
+	writeln('Fecha: ',v.fecha);
+	writeln('Unidades vendidas: ',v.unidadesVendidas);
 	writeln;
 end;
 
@@ -84,7 +84,8 @@ begin
 	
 end;
 
-procedure generarArbol1(var a:arbol);		//A - 1 - 3
+procedure generarArbol1(var a:arbol);		//A - 1 - 3	//Este arbol almacena los codigos distintos por la izquierda y las iguales a la derecha
+			
 var
 	v:venta;
 begin
@@ -94,18 +95,6 @@ begin
 		generarVenta(v);
 	end;
 end;
-
-
-procedure recorrerArbol(a:arbol);
-begin
-	if(a<>nil)then begin
-		recorrerArbol(a^.HI);
-		imprimirVenta(a^.dat);
-		recorrerArbol(a^.HD);
-	end;
-end;
-
-
 
 procedure insertarElem2(var a:arbol; v:venta);
 begin
@@ -118,7 +107,7 @@ begin
 	else a^.dat.unidadesVendidas := a^.dat.unidadesVendidas + v.unidadesVendidas;
 end;
 
-procedure generarArbol2(var a:arbol);		//A - 2
+procedure generarArbol2(var a:arbol);		//A - 2		Este arbol guarda en los codigos repetidos la suma de las unidades vendidas(No tiene codigos repetidos)
 var
 	v:venta;
 begin
@@ -129,18 +118,122 @@ begin
 	end;
 end;
 
+procedure retornarArboles(var a1,a2,a3:arbol);
+begin
+	generarArbol1(a1);
+	generarArbol2(a2);
+	generarArbol1(a3);
+end;
+
+procedure imprimirArbol(a:arbol);
+begin
+	if(a <> nil)then begin
+		imprimirArbol(a^.HI);
+		imprimirVenta(a^.dat);
+		imprimirArbol(a^.HD);
+	end;
+end;
+
+//B
+function sumarCantidadesFecha(a:arbol; f:integer):integer;
+begin
+	if(a = nil)then sumarCantidadesFecha:=0
+	else begin
+		if(a^.dat.fecha = f)then sumarCantidadesFecha := a^.dat.unidadesVendidas + sumarCantidadesFecha(a^.HI,f) + sumarCantidadesFecha(a^.HD,f)
+		else sumarCantidadesFecha := sumarCantidadesFecha(a^.HI,f) + sumarCantidadesFecha(a^.HD,f) 
+	end;
+end;
+
+//C
+
+procedure actualizarMaximo(var maxValor,maxElem : integer; nuevoValor, nuevoElem : integer);
+begin
+	if (nuevoValor >= maxValor) then
+	begin
+		maxValor := nuevoValor;
+		maxElem := nuevoElem;
+	end;
+end;
+	
+procedure codigoMasVenta(a: arbol; var maxCodigo: integer; var maxNum: integer);
+begin
+	if (a <> nil) then
+	begin
+		actualizarMaximo(maxNum,maxCodigo,a^.dat.unidadesVendidas,a^.dat.codigoProducto);
+		codigoMasVenta(a^.HI, maxCodigo,maxNum);
+		codigoMasVenta(a^.HD, maxCodigo,maxNum);
+	end; 
+end;
+
+procedure obtenerMaximo(a:arbol);
+var
+	maxNum,maxCod:integer;
+begin
+	if(a <> nil)then begin
+		maxNum := -1;
+		codigoMasVenta(a,maxCod,maxNum);
+		writeln(maxCod);
+		writeln(maxNum);
+	end;
+end;
+
+
+//D
+procedure recorrerHojaDerecha(a:arbol; var cant:integer);
+begin
+	while(a <> nil)do begin
+		cant := cant + 1;
+		a := a^.HD;
+	end;
+end;
+
+procedure obtenerMaximoCodVentas(a:arbol; var maxNum,maxCodigo:integer);
+var
+	cant:integer;
+begin
+	if(a <> nil)then begin
+		cant := 1;
+		recorrerHojaDerecha(a^.HD,cant);
+
+		actualizarMaximo(maxNum,maxCodigo,cant,a^.dat.codigoProducto);
+		
+		obtenerMaximoCodVentas(a^.HI,maxNum,maxCodigo);
+	end;
+end;
+
+procedure retornarMaxCodVentas(a:arbol);
+var
+	maxNum,maxCodigo:integer;
+begin
+	maxNum:=0;
+	obtenerMaximoCodVentas(a,maxNum,maxCodigo);
+	writeln('Codigo Max: ', maxCodigo);
+end;
 
 
 
 VAR
-	a1,a2:arbol;
+	a1,a2,a3:arbol;
+	f:integer;
 BEGIN
 	randomize;
 	a1:=nil;
 	a2:=nil;
-	generarArbol1(a1);
-	recorrerArbol(a1);
+	a3:=nil;
+	retornarArboles(a1,a2,a3);
+	imprimirArbol(a1);
+	
+	
 	writeln;
-	generarArbol2(a2);
-	recorrerArbol(a2);
+	write('Ingrese fecha: ');
+	readln(f);
+	writeln('Cantidad total vendida en ese fecha: ',sumarCantidadesFecha(a1,f));
+	
+	writeln;
+	imprimirArbol(a2);
+	obtenerMaximo(a2);
+	
+	writeln;
+	imprimirArbol(a3);
+	retornarMaxCodVentas(a3);
 END.
